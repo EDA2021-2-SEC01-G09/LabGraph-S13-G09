ebad4739b72bec2953924375b10aca6511903395
nmerchan2021 <n.merchan@uniandes.edu.co>
2021-11-17 12:16:31 -0500

    laboratorio 9 - Entrega final

--- App/view.py
+++ App/view.py
@@ -31,6 +31,7 @@ import threading
 from App import controller
 from DISClib.ADT import stack
 assert config
+import time
 
 """
 La vista se encarga de la interacción con el usuario.
@@ -83,7 +84,13 @@ def optionThree(cont):
 
 
 def optionFour(cont, initialStation):
+    start_time = time.process_time()
+
     controller.minimumCostPaths(cont, initialStation)
+
+    stop_time = time.process_time()
+    elapsed_time = (stop_time - start_time)*1000 
+    print('Tiempo requerido (mseg):', elapsed_time)
 
 
 def optionFive(cont, destStation):
@@ -94,6 +101,8 @@ def optionFive(cont, destStation):
 
 
 def optionSix(cont, destStation):
+    start_time = time.process_time()
+
     path = controller.minimumCostPath(cont, destStation)
     if path is not None:
         pathlen = stack.size(path)
@@ -103,6 +112,10 @@ def optionSix(cont, destStation):
             print(stop)
     else:
         print('No hay camino')
+
+    stop_time = time.process_time()
+    elapsed_time = (stop_time - start_time)*1000 
+    print('Tiempo requerido (mseg):', elapsed_time)
 
 
 def optionSeven(cont):
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ISIS1225-SampleGraph
+
+Integrantes:
+
+
+-Nombre: Nicolas Merchan Cuestas
+
+-Correo: 202112109
+
+-Código: 202112109
